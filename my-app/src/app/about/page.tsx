31638a7e8487630c0896b920ac78fa63586af502
MudasirIrshad <mudasirirshad47@gmail.com>
2025-02-19 00:06:20 +0500

    working on sociall app

---
+++ my-app/src/app/about/page.tsx
@@ -0,0 +1,5 @@
+function AboutPage() {
+  return <div>ABout page</div>;
+}
+
+export default AboutPage;
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,18 @@
+import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ModeToggle";
+export default function Home() {
+  return (
+    <div className="m-4">
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button variant={"secondary"}>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <ModeToggle />
+    </div>
+  );
+}
